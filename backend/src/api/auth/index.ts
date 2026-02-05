08556a7b02243838ea3e4527a7e41859380416ef
Neeraj Gupta <theneerajgupta@gmail.com>
2026-02-05 16:04:26 +0530

    placeholder files created

---
+++ backend/src/api/auth/index.ts
@@ -0,0 +1,8 @@
+// export * from './schemas/forgotPassword.schema';
+// export * from './schemas/login.schema';
+// export * from './schemas/logout.schema';
+// export * from './schemas/me.schema';
+// export * from './schemas/refresh.schema';
+// export * from './schemas/register.schema';
+// export * from './schemas/resetPassword.schema';
+// export * from './schemas/verifyEmail.schema';
